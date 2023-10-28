162392d12d9e94a36275de3d0f6b3a6d3ea92da4
Andre Luis <anddreluis98@gmail.com>
2023-10-28 02:01:40 -0300

    added navbar and shadcn

---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@/components/ui/button";
+
+export default function Header() {
+  return (
+    <header className="w-[100%] sticky top-0 z-50 px-24 justify-between items-center bg-gray-500 flex max-w-full h-[100px]">
+      <h1>VetSolutions</h1>
+      <div className="flex text-white gap-8 text-sm justify-center w-[1050px] items-center">
+        <a>HOME</a>
+        <a>SOBRE NÓS</a>
+        <a>O QUE OFERECEMOS</a>
+        <a>NOSSO DIFERENCAL</a>
+        <a>PLANOS</a>
+        <a>BLOG</a>
+        <Button className="bg-purple-400">Entre em contato</Button>
+      </div>
+    </header>
+  );
+}
