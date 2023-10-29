425e99f1754e9f55a51102c91d58e48d15c3c5b3
Andre Luis <anddreluis98@gmail.com>
2023-10-29 19:01:27 -0300

    add logo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,22 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Header() {
+  const logo = "/images/logo.png";
+  const VetSolutions = "/images/vetsolutions.svg";
   return (
-    <header className="w-[100%] sticky top-0 z-50 px-24 justify-between items-center bg-gray-500 flex max-w-full h-[100px]">
-      <h1>VetSolutions</h1>
+    <header className="w-[100%] sticky top-0 z-50 px-24 justify-between items-center bg-[#1E1E1E]/75 translucide flex max-w-full h-[100px]">
+      <div className="flex gap-2">
+        {" "}
+        <Image width={48} height={60} src={logo} alt="logo" />
+        <Image
+          className="mt-0"
+          width={200}
+          height={80}
+          src={VetSolutions}
+          alt="logo"
+        />
+      </div>
       <div className="flex text-white gap-8 text-xs justify-center w-[1050px] items-center">
         <a>HOME</a>
         <a>SOBRE NÓS</a>
@@ -11,7 +24,7 @@ export default function Header() {
         <a>NOSSO DIFERENCAL</a>
         <a>PLANOS</a>
         <a>BLOG</a>
-        <Button className="bg-purple font-bold underline rounded-full hover:bg-purple/75">
+        <Button className="bg-purple text-mint font-bold underline rounded-full hover:bg-purple/75">
           Entre em contato
         </Button>
       </div>
