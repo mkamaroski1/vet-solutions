0012c17ea5bcfc25b94943d0484cd690c3256f72
Andre Luis <anddreluis98@gmail.com>
2023-10-29 19:09:30 -0300

    set dynamic links

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,19 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Header() {
   const logo = "/images/logo.png";
   const VetSolutions = "/images/vetsolutions.svg";
+  const links = [
+    { text: "HOME", href: "/" },
+    { text: "SOBRE NÓS", href: "/sobre-nos" },
+    { text: "O QUE OFERECEMOS", href: "/o-que-oferecemos" },
+    { text: "NOSSO DIFERENCIAL", href: "/nosso-diferencial" },
+    { text: "PLANOS", href: "/planos" },
+    { text: "BLOG", href: "/blog" },
+  ];
+
   return (
     <header className="w-[100%] sticky top-0 z-50 px-24 justify-between items-center bg-[#1E1E1E]/75 translucide flex max-w-full h-[100px]">
       <div className="flex gap-2">
@@ -18,13 +28,12 @@ export default function Header() {
         />
       </div>
       <div className="flex text-white gap-8 text-xs justify-center w-[1050px] items-center">
-        <a>HOME</a>
-        <a>SOBRE NÓS</a>
-        <a>O QUE OFERECEMOS</a>
-        <a>NOSSO DIFERENCAL</a>
-        <a>PLANOS</a>
-        <a>BLOG</a>
-        <Button className="bg-purple text-mint font-bold underline rounded-full hover:bg-purple/75">
+        {links.map((link, index) => (
+          <Link key={index} href={link.href}>
+            {link.text}
+          </Link>
+        ))}
+        <Button className="bg-purple text-mint font-semibold hover:underline rounded-full hover:bg-purple/75">
           Entre em contato
         </Button>
       </div>
