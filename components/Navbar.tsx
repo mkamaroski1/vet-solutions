89ff86cfbe48d890e12cd64ba6b149fbbe58e571
Guilherme Padilha <guilhermepjobins@hotmail.com>
2023-12-13 20:26:59 -0300

    altlogo

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,30 +1,30 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
-import Link from "next/link";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Header() {
-  const logo = "/images/logo.png";
-  const VetSolutions = "/images/vetsolutions.svg";
+  const logo = '/images/logo.png';
+  const VetSolutions = '/images/vetsolutions.svg';
   const links = [
-    { text: "HOME", href: "/" },
-    { text: "SOBRE NÓS", href: "/sobre-nos" },
-    { text: "O QUE OFERECEMOS", href: "/o-que-oferecemos" },
-    { text: "NOSSO DIFERENCIAL", href: "/nosso-diferencial" },
-    { text: "PLANOS", href: "/planos" },
-    { text: "BLOG", href: "/blog" },
+    { text: 'HOME', href: '/' },
+    { text: 'SOBRE NÓS', href: '/sobre-nos' },
+    { text: 'O QUE OFERECEMOS', href: '/o-que-oferecemos' },
+    { text: 'NOSSO DIFERENCIAL', href: '/nosso-diferencial' },
+    { text: 'PLANOS', href: '/planos' },
+    { text: 'BLOG', href: '/blog' },
   ];
 
   return (
     <header className="w-[100%] sticky top-0 z-50 px-24 justify-between items-center bg-[#1E1E1E]/75 translucide flex max-w-full h-[80px]">
       <div className="flex gap-2">
-        {" "}
+        {' '}
         <Image width={48} height={60} src={logo} alt="logo" />
         <Image
           className="mt-0"
           width={200}
           height={80}
           src={VetSolutions}
-          alt="logo"
+          alt="vetSolution"
         />
       </div>
       <div className="flex text-white gap-8 text-xs justify-center w-[1050px] items-center">
