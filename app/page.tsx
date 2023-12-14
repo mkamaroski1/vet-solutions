77df234c8fd3b764ede79aab11310ce893c20e65
Guilherme Padilha <guilhermepjobins@hotmail.com>
2023-12-14 19:23:41 -0300

    add about

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
-import Navbar from "@/components/Navbar";
+import Navbar from '@/components/Navbar';
+import About from '@/components/About';
+import { aboutData } from '@/components/About-data';
 
 export default function Home() {
   return (
-    <main className="flex bg-[url('/images/background.png')] min-h-screen flex-col relative items-center justify-between">
-      <Navbar />
+    <main className="flex bg-[url('/images/background.png')] min-h-screen flex-col relative justify-between">
+      <div className="items-center">
+        <Navbar />
+      </div>
+      <About title={aboutData.title} />
     </main>
   );
 }
