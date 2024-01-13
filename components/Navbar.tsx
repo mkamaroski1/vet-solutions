30fad900af3ac205332529726600cb34bb635bee
Andre Luis <anddreluis98@gmail.com>
2024-01-13 16:38:15 -0300

    Refactor Navbar component

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,34 +1,34 @@
-'use client'
+"use client";
 
-import { useState, useEffect, useRef } from 'react';
-import Image from 'next/image';
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import { useState, useEffect, useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const ref = useRef();
-  const logo = '/images/Logo.svg';
-  const VetSolutions = '/images/vetsolutions.svg';
+  const logo = "/images/Logo.svg";
+  const VetSolutions = "/images/vetsolutions.svg";
   const links = [
-    { text: 'HOME', href: '/' },
-    { text: 'SOBRE NÓS', href: '/sobre-nos' },
-    { text: 'O QUE OFERECEMOS', href: '/o-que-oferecemos' },
-    { text: 'NOSSO DIFERENCIAL', href: '/nosso-diferencial' },
-    { text: 'PLANOS', href: '/planos' },
-    { text: 'BLOG', href: '/blog' },
+    { text: "HOME", href: "/" },
+    { text: "SOBRE NÓS", href: "/sobre-nos" },
+    { text: "O QUE OFERECEMOS", href: "/o-que-oferecemos" },
+    { text: "NOSSO DIFERENCIAL", href: "/nosso-diferencial" },
+    { text: "PLANOS", href: "/planos" },
+    { text: "BLOG", href: "/blog" },
   ];
 
   // Fechar o menu ao clicar fora dele
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (ref.current && !ref.current.contains(event.target)) {
-        setIsMenuOpen(false);
-      }
-    }
-    document.addEventListener('mousedown', handleClickOutside);
-    return () => document.removeEventListener('mousedown', handleClickOutside);
-  }, [ref]);
+  // useEffect(() => {
+  //   function handleClickOutside(event: () => void): void {
+  //     if (ref.current && !ref.current.contains(event.target)) {
+  //       setIsMenuOpen(false);
+  //     }
+  //   }
+  //   document.addEventListener("mousedown", handleClickOutside);
+  //   return () => document.removeEventListener("mousedown", handleClickOutside);
+  // }, [ref]);
 
   return (
     <header className="w-full sticky top-0 z-50 px-6 flex items-center bg-[#1E1E1E]/75 h-[80px] md:px-24">
@@ -60,8 +60,16 @@ export default function Header() {
       </div>
 
       {/* Sidebar para Mobile */}
-      <div className={`fixed inset-y-0 left-0 transform ${isMenuOpen ? "translate-x-0" : "-translate-x-full"} transition-transform duration-300 ease-in-out bg-[#1E1E1E]/75 p-4 w-64 h-full md:hidden z-50`} ref={ref}>
-        <button onClick={() => setIsMenuOpen(false)} className="absolute top-4 right-4">
+      <div
+        className={`fixed inset-y-0 left-0 transform ${
+          isMenuOpen ? "translate-x-0" : "-translate-x-full"
+        } transition-transform duration-300 ease-in-out bg-[#1E1E1E]/75 p-4 w-64 h-full md:hidden z-50`}
+        // ref={ref}
+      >
+        <button
+          onClick={() => setIsMenuOpen(false)}
+          className="absolute top-4 right-4"
+        >
           <svg viewBox="0 0 24 24" width="24" height="24" fill="#FFFFFF">
             <path d="M18 6L6 18M6 6l12 12" />
           </svg>
@@ -74,7 +82,6 @@ export default function Header() {
           ))}
         </div>
       </div>
-      
     </header>
   );
 }
