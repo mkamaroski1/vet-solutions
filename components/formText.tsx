8eb16178d2bf81dcb2e1476aa6a3b364a3412459
Andre Luis <anddreluis98@gmail.com>
2024-01-14 00:28:19 -0300

    Refactor WhatsApp component and update formText styling

--- components/formText.tsx
+++ components/formText.tsx
@@ -1,34 +1,34 @@
-import { AiOutlineWhatsApp } from 'react-icons/ai';
+import { AiOutlineWhatsApp } from "react-icons/ai";
 
 export const Info = () => {
   return (
     <div>
-      <h1 className="text-2xl font-bold flex items-center justify-center">
+      <h1 className="text-2xl font-bold mb-[1rem] flex items-center justify-center">
         INFORMAÇÕES E <br /> ORÇAMENTOS
       </h1>
-      <p className="text-sm mt-[1rem]">
+      <p className="text-md text-center mt-[1rem]">
         Preencha o formulario ou clique no link abaixo para entrar em contato
         com a nossa equipe
       </p>
     </div>
   );
 };
-const numberWhatsApp = '(41)12345-6789';
+const whatsAppNumber = "(41)12345-6789";
 
 export const WhatsApp = () => {
   return (
     <>
-      <div className="text-4xl items-stretch ">
-        <AiOutlineWhatsApp className="w-[4rem] h-[4rem]" />
-        <p>Entre em contato no nosso WhatsApp</p>
+      <div className="flex justify-center text-center gap-2 mb-[2rem] text-4xl">
+        <AiOutlineWhatsApp size={42} />
+        <p className="text-[24px]">Entre em contato no nosso WhatsApp</p>
       </div>
       <a
-        href={`https://wa.me/${numberWhatsApp}`}
+        href={`https://wa.me/${whatsAppNumber}`}
         target="_blank"
         rel="noopener noreferrer"
-        className="text-4xl items-center justify-center flex"
+        className="text-4xl pb-[1rem] underline items-center justify-center flex"
       >
-        (41)12345-6789
+        {whatsAppNumber}
       </a>
     </>
   );
